f0648ac25cb6226af00c22641c3cd716de36f796
William Willden <william@protoman.localdomain>
2018-03-30 18:17:05 -0600

    added postgres

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var cool = require('cool-ascii-faces');
+var pg = require('pg');
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -10,5 +11,16 @@ express()
   .get('/', (req, res) => res.render('pages/index'))
   .get('/cool', function(req, res) {
     res.send(cool());
+  })
+  .get('/db', function (request, response) {
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+      client.query('SELECT * FROM test_table', function(err, result) {
+        done();
+        if (err)
+        { console.error(err); response.send("Error " + err); }
+        else
+        { response.render('pages/db', {results: result.rows} ); }
+      });
+    });
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
